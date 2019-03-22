a984bfa0606240b0c1e194f36a41a2e412f9d9a3
ttsmyd <dmitry4884@yandex.ru>
2019-03-22 13:08:05 +0300

    [TG-75] Disable temlate for social component for building app

--- src/app/social/social.component.ts
+++ src/app/social/social.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { environment } from '../../environments/environment';
-
-@Component({
-  selector: 'app-social',
-  templateUrl: './social.component.html',
-  styleUrls: ['./social.component.css']
-})
-export class SocialComponent implements OnInit {
-    token: string;
-
-    API_BASE_URL = environment.apiUrl;
-    OAUTH2_REDIRECT_URI = environment.oauthRedirectUrl;
-
-    GOOGLE_AUTH_URL = this.API_BASE_URL + 'oauth2/authorize/google?redirect_uri=' + this.OAUTH2_REDIRECT_URI;
-    FACEBOOK_AUTH_URL = this.API_BASE_URL + 'oauth2/authorize/facebook?redirect_uri=' + this.OAUTH2_REDIRECT_URI;
-    GITHUB_AUTH_URL = this.API_BASE_URL + 'oauth2/authorize/github?redirect_uri=' + this.OAUTH2_REDIRECT_URI;
-
-    constructor() { }
-
-    ngOnInit() {
-    }
-
-}
+// import { Component, OnInit } from '@angular/core';
+// import { environment } from '../../environments/environment';
+//
+// @Component({
+//   selector: 'app-social',
+//   templateUrl: './social.component.html',
+//   styleUrls: ['./social.component.css']
+// })
+// export class SocialComponent implements OnInit {
+//     token: string;
+//
+//     API_BASE_URL = environment.apiUrl;
+//     OAUTH2_REDIRECT_URI = environment.oauthRedirectUrl;
+//
+//     GOOGLE_AUTH_URL = this.API_BASE_URL + 'oauth2/authorize/google?redirect_uri=' + this.OAUTH2_REDIRECT_URI;
+//     FACEBOOK_AUTH_URL = this.API_BASE_URL + 'oauth2/authorize/facebook?redirect_uri=' + this.OAUTH2_REDIRECT_URI;
+//     GITHUB_AUTH_URL = this.API_BASE_URL + 'oauth2/authorize/github?redirect_uri=' + this.OAUTH2_REDIRECT_URI;
+//
+//     constructor() { }
+//
+//     ngOnInit() {
+//     }
+//
+// }
+//
